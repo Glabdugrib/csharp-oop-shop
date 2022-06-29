736dc9abc295cd5658701d4472f45be52d066b2e
Simone Sada <si.sada93@gmail.com>
2022-06-29 14:29:15 +0200

    Exercise complete

--- Prodotto.cs
+++ Prodotto.cs
@@ -8,6 +8,49 @@ namespace csharp_oop_shop
 {
     internal class Prodotto
     {
-        
+
+        public int Codice 
+        { 
+            get;
+            private set;
+        }
+
+        public string Nome { get; set; }
+
+        public string Descrizione { get; set; }
+
+        public float Prezzo { get; set;  }
+
+        public float Iva { get; set; }
+
+        public Prodotto(string nome, string descrizione, float prezzo, float iva)
+        {
+            Random random = new Random();
+            Codice = random.Next(1, 1000);
+            Nome = nome;
+            Descrizione = descrizione;
+            Prezzo = prezzo;
+            Iva = iva;
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine("Istanza di Prodotto");
+            Console.WriteLine($"Codice: {Codice}");
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Descrizione: {Descrizione}");
+            Console.WriteLine($"Prezzo: {Prezzo} euro");
+            Console.WriteLine($"Iva: {Iva}%");
+        }
+
+        public string getNomeEsteso()
+        {
+            return Codice + Nome;
+        }
+
+        public float getPrezzoIva()
+        {
+            return Prezzo * (1 + Iva);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿namespace csharp_oop_shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Prodotto prod1 = new Prodotto("Libro", "Libro molto interessante", 19.99f, 2f);
+            prod1.ToString();
+        }
+    }
+}
